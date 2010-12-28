de58e65c2193aa45a821d96da46b956a2cf2b6a0
Andreia Gaita <shana@jitted.com>
2010-12-28 17:34:56 +0000

    testing qt

---
+++ tests/qttests/Main.cs
@@ -0,0 +1,13 @@
+using System;
+using Qt.Gui;
+using Mono.VisualC.Interop;
+
+namespace qttests {
+	class MainClass {
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
+
