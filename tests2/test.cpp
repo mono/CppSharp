27ea615c3dca4465f6fce8441e909b6b59639411
Zoltan Varga <vargaz@gmail.com>
2011-02-28 18:04:47 +0100

    Add regression tests for the c++ binding.

---
+++ tests2/test.cpp
@@ -0,0 +1,36 @@
+
+#include "test.h"
+
+ClassWithCopyCtor::ClassWithCopyCtor(const ClassWithCopyCtor& f) {
+	x = f.x;
+}
+
+ClassWithCopyCtor
+ClassWithCopyCtor::Return (int x) {
+	return ClassWithCopyCtor (x);
+}
+
+int
+ClassWithCopyCtor::GetX () {
+	return x;
+}
+
+ClassWithDtor
+ClassWithDtor::Return (int x) {
+	return ClassWithDtor (x);
+}
+
+int
+ClassWithDtor::GetX () {
+	return x;
+}
+
+ClassWithoutCopyCtor
+ClassWithoutCopyCtor::Return (int x) {
+	return ClassWithoutCopyCtor (x);
+}
+
+int
+ClassWithoutCopyCtor::GetX () {
+	return x;
+}
