mono/CppSharp

commit 65edb5389272392fb56bbc82305cd89465da1e0e

Fix issues when base class virtual methods are prepended into primary vtable.

src/Mono.VisualC.Interop/Util/LazyGeneratedList.cs
@@ -37,13 +37,16 @@ namespace Mono.VisualC.Interop.Util {
 		private TItem [] cache;
 		private Func<int, TItem> generator;
 
-		private int skew;
+		private LazyGeneratedList<TItem> previous;
+		private LazyGeneratedList<TItem> next;
+		private int lead, follow;
 
 		public LazyGeneratedList (int count, Func<int, TItem> generator)
 		{
 			this.cache = new TItem [count];
 			this.generator = generator;
-			this.skew = 0;
+			this.lead = 0;
+			this.follow = 0;
 		}
 
 		public IEnumerator<TItem> GetEnumerator ()
@@ -57,7 +60,7 @@ namespace Mono.VisualC.Interop.Util {
 		}
 
 		public int Count {
-			get { return cache.Length + skew; }
+			get { return lead + cache.Length + follow; }
 		}
 
 		public bool IsReadOnly {
@@ -66,8 +69,19 @@ namespace Mono.VisualC.Interop.Util {
 
 		public TItem this [int index] {
 			get {
-				int realIndex = index - skew;
-				if (realIndex < 0) return null;
+				if (index < lead) {
+					if (previous != null && index > 0)
+						return previous [index];
+					throw new IndexOutOfRangeException (index.ToString ());
+				}
+
+				int realIndex = index - lead;
+				if (realIndex > cache.Length) {
+					int followIndex = realIndex - cache.Length;
+					if (next != null && followIndex < follow)
+						return next [followIndex];
+					throw new IndexOutOfRangeException (index.ToString ());
+				}
 
 				if (cache [realIndex] == null)
 					cache [realIndex] = generator (realIndex);
@@ -79,9 +93,49 @@ namespace Mono.VisualC.Interop.Util {
 			}
 		}
 
-		public void Skew (int skew)
+		/* Visual aid for the behavior of the following methods:
+
+			|<Prepended Range><Generated Range><Appended Range>|
+                         ^               ^                 ^              ^
+                         PrependFirst    PrependLast       AppendFirst    AppendLast
+		*/
+
+		public void AppendFirst (LazyGeneratedList<TItem> list)
 		{
-			this.skew += skew;
+			follow += list.Count;
+			if (next != null)
+				list.AppendLast (next);
+
+			next = list;
+		}
+
+		public void AppendLast (LazyGeneratedList<TItem> list)
+		{
+			if (next == null)
+				next = list;
+			else
+				next.AppendLast (list);
+
+			follow += list.Count;
+		}
+
+		public void PrependFirst (LazyGeneratedList<TItem> list)
+		{
+			if (previous == null)
+				previous = list;
+			else
+				previous.PrependFirst (list);
+
+			lead += list.Count;
+		}
+
+		public void PrependLast (LazyGeneratedList<TItem> list)
+		{
+			lead += list.Count;
+			if (previous != null)
+				list.PrependFirst (previous);
+
+			previous = list;
 		}
 
 		// FIXME: Should probably implement these 3 at some point

src/Mono.VisualC.Interop/CppTypeInfo.cs
@@ -103,8 +103,8 @@ namespace Mono.VisualC.Interop {
 					VirtualMethods.Insert (BaseVTableSlots + i, baseType.VirtualMethods [i]);
 	
 				BaseVTableSlots += newVirtualMethodCount;
-				VTableDelegateTypes.Skew (newVirtualMethodCount);
-				VTableOverrides.Skew (newVirtualMethodCount);
+				VTableDelegateTypes.PrependLast (baseType.VTableDelegateTypes);
+				VTableOverrides.PrependLast (baseType.VTableOverrides);
 			}
 
 			field_offset_padding += baseType.native_size +

commit 2116e1f1756af74345f30deaacde887e76fa44f0

Fix Itanium bugs introduced by commit 65edb53

src/Mono.VisualC.Interop/ABI/Impl/ItaniumTypeInfo.cs
@@ -41,6 +41,8 @@ namespace Mono.VisualC.Interop.ABI {
 
 				hasVirtualDtor = true;
 				VirtualMethods.RemoveAt (i);
+				VTableDelegateTypes.RemoveAt (i);
+				VTableOverrides.RemoveAt (i);
 				break;
 			}
 		}
@@ -58,7 +60,7 @@ namespace Mono.VisualC.Interop.ABI {
 				return;
 
 			hasVirtualDtor |= ((ItaniumTypeInfo)baseType).HasVirtualDestructor;
-			base.AddBase (baseType);
+			base.AddBase (baseType, false);
 		}
 
 		// Itanium puts all its virtual dtors at the bottom of the vtable (2 slots each).

src/Mono.VisualC.Interop/Util/LazyGeneratedList.cs
@@ -39,14 +39,18 @@ namespace Mono.VisualC.Interop.Util {
 
 		private LazyGeneratedList<TItem> previous;
 		private LazyGeneratedList<TItem> next;
-		private int lead, follow;
+		private int lead, content, follow;
+
+		private HashSet<int> removed;
 
 		public LazyGeneratedList (int count, Func<int, TItem> generator)
 		{
 			this.cache = new TItem [count];
 			this.generator = generator;
 			this.lead = 0;
+			this.content = count;
 			this.follow = 0;
+			this.removed = new HashSet<int> ();
 		}
 
 		public IEnumerator<TItem> GetEnumerator ()
@@ -60,7 +64,7 @@ namespace Mono.VisualC.Interop.Util {
 		}
 
 		public int Count {
-			get { return lead + cache.Length + follow; }
+			get { return lead + content + follow; }
 		}
 
 		public bool IsReadOnly {
@@ -69,35 +73,40 @@ namespace Mono.VisualC.Interop.Util {
 
 		public TItem this [int index] {
 			get {
-				if (index < lead) {
-					if (previous != null && index > 0)
-						return previous [index];
-					throw new IndexOutOfRangeException (index.ToString ());
-				}
-
-				int realIndex = index - lead;
-				if (realIndex > cache.Length) {
-					int followIndex = realIndex - cache.Length;
-					if (next != null && followIndex < follow)
-						return next [followIndex];
-					throw new IndexOutOfRangeException (index.ToString ());
-				}
-
-				if (cache [realIndex] == null)
-					cache [realIndex] = generator (realIndex);
-
-				return cache [realIndex];
+				return ForIndex (index, i => previous [i], i => (cache [i] == null? (cache [i] = generator (i)) : cache [i]), i => next [i]);
 			}
 			set {
 				throw new NotSupportedException ("This IList is read only");
 			}
 		}
 
+		private TItem ForIndex (int index, Func<int,TItem> leadAction, Func<int,TItem> contentAction, Func<int,TItem> followAction)
+		{
+			if (removed.Contains (index))
+				index++;
+
+			if (index < lead) {
+				if (previous != null && index >= 0)
+					return leadAction (index);
+				throw new IndexOutOfRangeException (index.ToString ());
+			}
+
+			int realIndex = index - lead;
+			if (realIndex >= content) {
+				int followIndex = realIndex - content;
+				if (next != null && followIndex < follow)
+					return followAction (followIndex);
+				throw new IndexOutOfRangeException (index.ToString ());
+			}
+
+			return contentAction (realIndex);
+		}
+
 		/* Visual aid for the behavior of the following methods:
 
 			|<Prepended Range><Generated Range><Appended Range>|
-                         ^               ^                 ^              ^
-                         PrependFirst    PrependLast       AppendFirst    AppendLast
+			 ^               ^                 ^              ^
+			 PrependFirst    PrependLast       AppendFirst    AppendLast
 		*/
 
 		public void AppendFirst (LazyGeneratedList<TItem> list)
@@ -159,7 +168,11 @@ namespace Mono.VisualC.Interop.Util {
 		}
 		public void RemoveAt (int index)
 		{
-			throw new NotImplementedException ();
+			while (removed.Contains (index))
+				index++;
+
+			removed.Add (index);
+			content--;
 		}
 		public void Add (TItem item)
 		{