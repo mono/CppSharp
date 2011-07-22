0d5dd661173af1794164a7dae5369e96c8a027a9
Alexander Corrado <alexander.corrado@gmail.com>
2011-07-22 03:04:03 -0400

    qt: add handwritten QString because generated version had problems

---
+++ src/qt/QString.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+using Mono.Cxxi;
+
+namespace Qt.Gui {
+	//TODO: Will this leak?
+	[StructLayout (LayoutKind.Sequential)]
+	public unsafe struct QString {
+		#region Sync with qstring.h
+		public interface IQString : ICppClass {
+			[Constructor] void QString(ref QString @this, [MangleAs ("const QChar*")] IntPtr unicode, int size);
+		}
+		
+		[StructLayout (LayoutKind.Sequential)]
+		public struct Data {
+			public int @ref;
+			public int alloc, size;
+			public IntPtr data;
+			public ushort clean;
+			public ushort simpletext;
+			public ushort righttoleft;
+			public ushort asciiCache;
+			public ushort capacity;
+			public ushort reserved;
+			public IntPtr array;
+		}
+		
+		
+		public Data* d;
+		#endregion
+		
+		private static IQString impl = Libs.QtGui.GetClass<IQString> ("QString");
+		
+		public QString (string str) : this ()
+		{
+			var strPtr = Marshal.StringToHGlobalUni (str);
+			impl.QString (ref this, strPtr, str.Length);
+			Marshal.FreeHGlobal (strPtr);
+			
+			// TODO: I deref this on construction to let Qt free it when it's done with it.
+			//  My assumption is that this struct will only be used to interop with Qt and
+			//  no managed class is going to hold on to it.
+			this.DeRef ();
+		}
+		
+		public static implicit operator QString (string str)
+		{
+			return new QString (str);
+		}
+		
+		public QString AddRef ()
+		{
+			d->@ref++;
+			return this;
+		}
+		
+		public QString DeRef ()
+		{
+			d->@ref--;
+			return this;
+		}
+	}
+}
+
--- src/qt/hello.cs
+++ src/qt/hello.cs
@@ -1,7 +1,7 @@
 using System;
 using Qt.Gui;
 
-using Mono.VisualC.Interop;
+using Mono.Cxxi;
 
 namespace QtTest {
 	class MainClass {
@@ -9,29 +9,11 @@ namespace QtTest {
 		{
 			int argc = args.Length;
 			using (QApplication app = new QApplication (ref argc, args, 0x040602)) {
-				QPushButton hello = new QPushButton (QString.FromUtf8 ("Hello", 5), null);
+				QPushButton hello = new QPushButton ("Hello", null);
 				hello.Resize (100, 30);
 				hello.Show ();
-				CppLibrary.SaveInteropAssembly ();
 				QApplication.Exec ();
 			}
-						/*
-				using (QPushButton hello = new QPushButton ("Hello world!"),
-						   hello2 = new QPushButton ("Another button")) {
-
-					hello.Resize (100, 30);
-					hello2.Resize (200, 30);
-
-					//CppLibrary.SaveInteropAssembly ();
-					hello.Visible = true;
-					hello2.Visible = true;
-
-					app.Exec ();
-
-
-				}
-			}
-						*/
 		}
 	}
 }
