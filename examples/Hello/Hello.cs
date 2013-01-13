68bfed9f3ae3a72990f21d047100731cadd4b10e
Tarmo Pikaro <tapika@yahoo.com>
2013-01-13 11:07:17 +0200

    Update test app.

--- examples/Hello/Hello.cs
+++ examples/Hello/Hello.cs
@@ -6,6 +6,8 @@ public class HelloExample
 	public static void Main (String[] args) {
 		var h = new Hello();
 		h.PrintHello ("Hello world");
+        Console.WriteLine("True =" + h.test1(3, 3.0f).ToString());
+        Console.WriteLine("False =" + h.test1(2, 3.0f).ToString());
 	}
 }
 
