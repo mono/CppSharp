870e8881738ffff5a81b6026c60b9b452cf33e41
triton <ripzonetriton@gmail.com>
2013-01-30 18:26:39 +0000

    Update the build bat to support VS 2012.

--- build/GenerateProjects.bat
+++ build/GenerateProjects.bat
@@ -6,7 +6,7 @@ echo Build Project Generator:
 echo.
 echo [0] Clean
 echo [1] Visual C++ 2010
-echo [2] Visual C++ 2008
+echo [2] Visual C++ 2012
 echo [3] CodeLite
 echo [4] GNU Make
 echo.
@@ -15,7 +15,7 @@ echo.
 set /P C="Choice: "
 if "%C%"=="4" goto gmake
 if "%C%"=="3" goto codelite
-if "%C%"=="2" goto vs2008
+if "%C%"=="2" goto vs2012
 if "%C%"=="1" goto vs2010
 if "%C%"=="0" goto clean
 
@@ -27,8 +27,8 @@ goto quit
 "premake4" --file=premake4.lua vs2010
 goto quit
 
-:vs2008
-"premake4" --file=premake4.lua vs2008
+:vs2012
+"premake4" --file=premake4.lua vs2012
 goto quit
 
 :codelite
