d56050803d724146f20dd47b50719a4d34e2dfdc
triton <ripzonetriton@gmail.com>
2013-03-14 12:15:49 +0000

    Extract the TranslationUnit class into its own file.

--- src/Bridge/Library.cs
+++ src/Bridge/Library.cs
@@ -1,6 +1,4 @@
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 
 namespace Cxxi
 {
@@ -15,57 +13,6 @@ namespace Cxxi
     {
         Present,
         Unavailable
-    }
-
-    /// <summary>
-    /// Represents a parsed C++ unit.
-    /// </summary>
-    [DebuggerDisplay("File = {FileName}, Ignored = {Ignore}")]
-    public class TranslationUnit : Namespace
-    {
-        public TranslationUnit(string file)
-        {
-            Macros = new List<MacroDefinition>();
-            FilePath = file;
-        }
-
-        /// Contains the macros present in the unit.
-        public List<MacroDefinition> Macros;
-
-        // Whether the unit should be generated.
-        public override bool IsGenerated
-        {
-            get { return !IgnoreFlags.HasFlag(IgnoreFlags.Generation); }
-        }
-
-        // Whether the unit should be processed.
-        public override bool IsProcessed
-        {
-            get { return !IgnoreFlags.HasFlag(IgnoreFlags.Processing); }
-        }
-
-        public bool IsSystemHeader { get; set; }
-        
-        /// Contains the path to the file.
-        public string FilePath;
-
-        /// Contains the name of the file.
-        public string FileName
-        {
-            get { return Path.GetFileName(FilePath); }
-        }
-
-        /// Contains the name of the module.
-        public string FileNameWithoutExtension
-        {
-            get { return Path.GetFileNameWithoutExtension(FileName); }
-        }
-
-        /// Contains the include path.
-        public string IncludePath;
-
-        /// Type references object.
-        public object TypeReferences;
     }
 
     /// <summary>
