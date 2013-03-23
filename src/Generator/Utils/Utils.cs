9d2da916ec17f38ee59ddb1cbab883bd34b455bf
marcos henrich <marcoshenrich@hotmail.com>
2013-03-23 15:34:11 +0000

    TextGenerator now indents multiple lines of the given string

--- src/Generator/Utils/Utils.cs
+++ src/Generator/Utils/Utils.cs
@@ -164,6 +164,17 @@ namespace Cxxi
             var str = Regex.Replace(input, "([A-Z])", " $1", RegexOptions.Compiled);
             return str.Trim().Split();
         }
+
+        public static IEnumerable<string> SplitAndKeep(this string s, string seperator)
+        {
+            string[] obj = s.Split(new string[] { seperator }, StringSplitOptions.None);
+
+            for (int i = 0; i < obj.Length; i++)
+            {
+                string result = i == obj.Length - 1 ? obj[i] : obj[i] + seperator;
+                yield return result;
+            }
+        }
     }
 
     public static class AssemblyHelpers
