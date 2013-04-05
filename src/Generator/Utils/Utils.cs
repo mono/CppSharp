42d3d0cc978005c3754d17548532ff70fae705f2
triton <ripzonetriton@gmail.com>
2013-04-05 03:17:37 +0100

    Added an utility method to uppercase the first letter of strings.

--- src/Generator/Utils/Utils.cs
+++ src/Generator/Utils/Utils.cs
@@ -175,6 +175,17 @@ namespace Cxxi
                 yield return result;
             }
         }
+
+        public static string UppercaseFirst(string s)
+        {
+            // Check for empty string.
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            // Return char and concat substring.
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
     }
 
     public static class AssemblyHelpers
