f738d00738e08aed4cd1b94a3071cd941c5f6428
marcos henrich <marcoshenrich@hotmail.com>
2013-07-19 17:57:50 +0100

    Parser now initializes ClassTemplate's parameters

--- src/Parser/Parser.cpp
+++ src/Parser/Parser.cpp
@@ -499,6 +499,17 @@ CppSharp::AST::ClassTemplate^ Parser::WalkClassTemplate(clang::ClassTemplateDecl
     auto Class = WalkRecordCXX(TD->getTemplatedDecl());
     CppSharp::AST::ClassTemplate^ CT = gcnew CppSharp::AST::ClassTemplate(Class);
 
+    auto TPL = TD->getTemplateParameters();
+    for(auto it = TPL->begin(); it != TPL->end(); ++it)
+    {
+        auto ND = *it;
+
+        auto TP = CppSharp::AST::TemplateParameter();
+        TP.Name = clix::marshalString<clix::E_UTF8>(ND->getNameAsString());
+
+        CT->Parameters->Add(TP);
+    }
+
     return CT;
 }
 
