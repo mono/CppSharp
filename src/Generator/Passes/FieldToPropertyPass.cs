379cf32f1de8a8b9ea698e0c8a8cdae6ebc0bb75
marcos henrich <marcoshenrich@hotmail.com>
2013-07-30 10:12:19 +0100

    Added FieldToPropertyPass, all non ignored fields can now be transformed into properties before generation.

---
+++ src/Generator/Passes/FieldToPropertyPass.cs
@@ -0,0 +1,33 @@
+﻿using CppSharp.AST;
+
+namespace CppSharp.Passes
+{
+    public class FieldToPropertyPass : TranslationUnitPass
+    {
+        public override bool VisitFieldDecl(Field field)
+        {
+            var @class = field.Namespace as Class;
+            if (@class == null)
+                return false;
+
+            if (@class.IsValueType)
+                return false;
+
+            if (ASTUtils.CheckIgnoreField(@class,field))
+                return false;
+
+            var prop = new Property()
+            {
+                Name = field.Name,
+                Namespace = field.Namespace,
+                QualifiedType = field.QualifiedType,
+                Field = field
+            };
+            @class.Properties.Add(prop);
+
+            field.ExplicityIgnored = true;
+
+            return false;
+        }
+    }
+}
