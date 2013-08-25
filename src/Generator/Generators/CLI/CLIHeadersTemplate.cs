0c64c06271d27077e2bbfb99daa2c2e8b5311bbf
triton <ripzonetriton@gmail.com>
2013-08-25 16:11:16 +0100

    Remove extra newline from the output.

--- src/Generator/Generators/CLI/CLIHeadersTemplate.cs
+++ src/Generator/Generators/CLI/CLIHeadersTemplate.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using CppSharp.AST;
-using CppSharp.Generators.CSharp;
 using CppSharp.Types;
 
 namespace CppSharp.Generators.CLI
@@ -632,7 +631,6 @@ namespace CppSharp.Generators.CLI
             }
             PopIndent();
 
-            NewLine();
             WriteLine("};");
 
             PopBlock(NewLineKind.BeforeNextBlock);
