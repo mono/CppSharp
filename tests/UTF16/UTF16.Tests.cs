6cb37ef382aa294609e0a6cc7b71d51903cbcffd
Dimitar Dobrev <dpldobrev@yahoo.com>
2013-09-13 19:42:17 +0300

    Added a test for indexers ([] operators).
    Signed-off-by: Dimitar Dobrev <dpldobrev@yahoo.com>

--- tests/UTF16/UTF16.Tests.cs
+++ tests/UTF16/UTF16.Tests.cs
@@ -1,5 +1,6 @@
-﻿using NUnit.Framework;
-using UTF16;
+﻿using System;
+using NUnit.Framework;
+using Foo = UTF16.Foo;
 
 [TestFixture]
 public class UTF16Tests
@@ -11,5 +12,9 @@ public class UTF16Tests
         const string georgia = "საქართველო";
         foo.Unicode = georgia;
         Assert.That(foo.Unicode, Is.EqualTo(georgia));
+
+        // TODO: move this, it has nothing to do with Unicode, it's here only not to break the CLI branch
+        Assert.That(foo[0], Is.EqualTo(5));
+        Assert.That(foo.OperatorSubscript(0), Is.EqualTo(IntPtr.Zero));
     }
 }
--- tests/UTF16/UTF16.cpp
+++ tests/UTF16/UTF16.cpp
@@ -1,1 +1,12 @@
 #include "UTF16.h"
+
+// TODO: move this, it has nothing to do with Unicode, it's here only not to break the CLI branch
+int Foo::operator[](int i) const
+{
+    return 5;
+}
+
+char* Foo::operator[](int i)
+{
+    return 0;
+}
