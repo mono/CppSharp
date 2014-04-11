429774d7591a885436ddbad78675a945948047b6
Elias Holzer <elias@vvvv.org>
2014-04-11 17:55:34 +0200

    Fixed retrieval of method overloads in case the provided function is an operator.

--- tests/Basic/Basic.h
+++ tests/Basic/Basic.h
@@ -324,3 +324,12 @@ namespace SomeNamespace
 		virtual void AbstractMethod() = 0;
 	};
 }
+
+// Test operator overloads
+class ClassWithOverloadedOperators
+{
+public:
+	operator char();
+	operator int();
+	operator short();
+};
