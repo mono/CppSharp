mono/CppSharp

commit d68b7b3b56a6274123cb91feaffc310ed09d0667

Fixed code generation for enum pointers as parameters.
Fixes issue #217.

src/Generator/Generators/CLI/CLITypePrinter.cs
@@ -129,7 +129,7 @@ namespace CppSharp.Generators.CLI
 
         public string VisitPointerType(PointerType pointer, TypeQualifiers quals)
         {
-            var pointee = pointer.Pointee;
+            var pointee = pointer.Pointee.Desugar();
 
             if (pointee is FunctionType)
             {
@@ -143,13 +143,20 @@ namespace CppSharp.Generators.CLI
             }
 
             PrimitiveType primitive;
-            if (pointee.Desugar().IsPrimitiveType(out primitive))
+            if (pointee.IsPrimitiveType(out primitive))
             {
                 var param = Context.Parameter;
                 if (param != null && (param.IsOut || param.IsInOut))
                     return VisitPrimitiveType(primitive);
 
                 return VisitPrimitiveType(primitive, quals) + "*";
+            }
+
+            Enumeration @enum;
+            if (pointee.IsTagDecl(out @enum))
+            {
+                var typeName = @enum.Visit(this);
+                return string.Format("{0}*", typeName);
             }
 
             return pointee.Visit(this, quals);

src/Generator/Generators/CLI/CLIMarshal.cs
@@ -421,6 +421,15 @@ namespace CppSharp.Generators.CLI
                 return VisitDelegateType(function, cppTypeName);
             }
 
+            Enumeration @enum;
+            if (pointee.IsTagDecl(out @enum))
+            {
+                ArgumentPrefix.Write("&");
+                Context.Return.Write("(::{0})*{1}", @enum.QualifiedOriginalName,
+                             Context.Parameter.Name);
+                return true;
+            }
+
             Class @class;
             if (pointee.IsTagDecl(out @class) && @class.IsValueType)
             {

src/Generator/Generators/CSharp/CSharpTypePrinter.cs
@@ -222,6 +222,12 @@ namespace CppSharp.Generators.CSharp
                 return VisitPrimitiveType(primitive, quals) + "*";
             }
 
+            Enumeration @enum;
+            if (desugared.IsTagDecl(out @enum))
+            {
+                return @enum.Name + "*";
+            }
+
             Class @class;
             if ((desugared.IsDependent || desugared.IsTagDecl(out @class))
                 && ContextKind == CSharpTypePrinterContextKind.Native)

tests/Basic/Basic.Tests.cs
@@ -257,5 +257,13 @@ public class BasicTests : GeneratorTestFixture
         prop.FieldValue = 10;
         Assert.That(prop.FieldValue, Is.EqualTo(10));
     }
+
+    [Test]
+    public unsafe void TestArraysPointers()
+    {
+        var values = MyEnum.A;
+        var arrays = new TestArraysPointers(&values, 1);
+        Assert.That(arrays.Value, Is.EqualTo(MyEnum.A));
+    }
 }
  

tests/Basic/Basic.h
@@ -350,3 +350,16 @@ struct DLL_API TestProperties
     int getFieldValue() { return Field; }
     void setFieldValue(int Value) { Field = Value; }
 };
+
+enum struct MyEnum { A, B, C };
+
+class DLL_API TestArraysPointers
+{
+public:
+    TestArraysPointers(MyEnum *values, int count)
+    {
+        if (values && count) Value = values[0];
+    }
+
+    MyEnum Value;
+};

commit c7dba1b5bd89d1871af12fed42876035a6dfbacc

Fixed type map matching regression introduced in d68b7b3.
Thanks to Elias Holzer for reporting the issue.

Conflicts:
	src/Generator/Generators/CLI/CLITypePrinter.cs

src/Generator/Generators/CLI/CLITypePrinter.cs
@@ -167,7 +167,7 @@ namespace CppSharp.Generators.CLI
                 return string.Format("{0}*", typeName);
             }
 
-            return pointee.Visit(this, quals);
+            return pointer.Pointee.Visit(this, quals);
         }
 
         public string VisitMemberPointerType(MemberPointerType member,