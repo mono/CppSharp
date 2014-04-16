d37822638f7cdb18bac965782ec43062dd030831
triton <ripzonetriton@gmail.com>
2014-04-16 18:58:03 +0100

    Split the parser-related build code into its own file.

--- build/premake4.lua
+++ build/premake4.lua
@@ -6,45 +6,8 @@ config = {}
 
 dofile "Helpers.lua"
 dofile "Tests.lua"
-
--- Setup the LLVM dependency
 dofile "LLVM.lua"
-
-newoption {
-     trigger = "parser",
-     description = "Controls which version of the parser is enabled.",
-     value = "version",
-     allowed = {
-          { "cpp", "Cross-platform C++ parser."},
-          { "cli", "VS-only C++/CLI parser."},
-     }
-}
-
-function SetupCLIParser()
-  local parser = _OPTIONS["parser"]
-  if not parser or parser == "cli" then
-    defines { "OLD_PARSER" }
-    links { "CppSharp.Parser" }
-  else
-    links { "CppSharp.Parser.CLI" }
-  end
-end
-
-function SetupCSharpParser()
-  links
-  {
-    "CppSharp.Parser.CSharp",
-    "CppSharp.Runtime"
-  }
-end
-
-function SetupParser()
-  if string.match(action, "vs*") then
-    SetupCLIParser()
-  else
-    SetupCSharpParser()
-  end
-end
+dofile "Parser.lua"
 
 solution "CppSharp"
 
@@ -77,7 +40,7 @@ solution "CppSharp"
 
   group "Examples"
     IncludeExamples()
-
+  
   end
   
   group "Tests"
