e85d8cd7ebedd25707e3a18369fa3b8dd93434cc
triton <joao@tritao.eu>
2014-05-04 20:55:59 +0100

    Fixed CDECL to actually attribute as `cdecl` and not `stdcall`.

--- tests/Tests.h
+++ tests/Tests.h
@@ -7,7 +7,7 @@
 #else
 #define DLL_API __attribute__ ((visibility ("default")))
 #define STDCALL __attribute__((stdcall))
-#define CDECL __attribute__((stdcall))
+#define CDECL __attribute__((cdecl))
 #endif
 
 #define CS_OUT
