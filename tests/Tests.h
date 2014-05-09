8c75289033623c366a6949adfabe4cdcf550d4ac
triton <joao@tritao.eu>
2014-05-09 14:50:25 +0100

    Check for defines before defining to get rid of some warnings.

--- tests/Tests.h
+++ tests/Tests.h
@@ -2,12 +2,24 @@
 
 #if defined(_MSC_VER)
 #define DLL_API __declspec(dllexport)
+
+#ifndef STDCALL
 #define STDCALL __stdcall
+#endif
+
+#ifndef CDECL
 #define CDECL __cdecl
+#endif
 #else
 #define DLL_API __attribute__ ((visibility ("default")))
+
+#ifndef STDCALL
 #define STDCALL __attribute__((stdcall))
+#endif
+
+#ifndef CDECL
 #define CDECL __attribute__((cdecl))
+#endif
 #endif
 
 #define CS_OUT
