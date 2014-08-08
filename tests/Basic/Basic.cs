5e7c70bb1e484221d0d4046a51506aa4ed302de8
Tom Spilman <tom@sickheadgames.com>
2014-08-07 19:30:27 -0500

    Changed generator to use CheckMacroPass.

--- tests/Basic/Basic.cs
+++ tests/Basic/Basic.cs
@@ -27,16 +27,9 @@ namespace CppSharp.Tests
         public override void Preprocess(Driver driver, ASTContext ctx)
         {
             driver.AddTranslationUnitPass(new GetterSetterToPropertyPass());
+            driver.AddTranslationUnitPass(new CheckMacroPass());
             ctx.SetClassAsValueType("Bar");
             ctx.SetClassAsValueType("Bar2");
-            ctx.SetMethodParameterUsage("Hello", "TestPrimitiveOut", 1, ParameterUsage.Out);
-            ctx.SetMethodParameterUsage("Hello", "TestPrimitiveOutRef", 1, ParameterUsage.Out);
-            ctx.SetMethodParameterUsage("Hello", "TestPrimitiveInOut", 1, ParameterUsage.InOut);
-            ctx.SetMethodParameterUsage("Hello", "TestPrimitiveInOutRef", 1, ParameterUsage.InOut);
-            ctx.SetMethodParameterUsage("Hello", "EnumOut", 2, ParameterUsage.Out);
-            ctx.SetMethodParameterUsage("Hello", "EnumOutRef", 2, ParameterUsage.Out);
-            ctx.SetMethodParameterUsage("Hello", "EnumInOut", 1, ParameterUsage.InOut);
-            ctx.SetMethodParameterUsage("Hello", "EnumInOutRef", 1, ParameterUsage.InOut);
         }
 
         public static void Main(string[] args)
