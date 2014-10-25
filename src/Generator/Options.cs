8c962c2cb711197dedd58d2d6ffd0effb8f13b32
triton <joao@tritao.eu>
2014-10-25 23:23:34 +0100

    Fixed the IsItaniumLikeAbi check.

--- src/Generator/Options.cs
+++ src/Generator/Options.cs
@@ -64,7 +64,7 @@ namespace CppSharp
         public bool IgnoreParseWarnings;
         public bool IgnoreParseErrors;
 
-        public bool IsItaniumLikeAbi { get { return Abi == CppAbi.Itanium || Abi == CppAbi.ARM; } }
+        public bool IsItaniumLikeAbi { get { return Abi != CppAbi.Microsoft; } }
         public bool IsMicrosoftAbi { get { return Abi == CppAbi.Microsoft; } }
 
         // Library options
