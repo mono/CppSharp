077e505cd7a68db3c459e4b3134b6b2f68dd9559
Abhinav Tripathi <genuineabhinav@gmail.com>
2015-07-26 19:12:03 +0530

    Added marshalling to struct...

---
+++ src/Generator/Passes/CheckValueTypeClassesPass.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using CppSharp.AST;
+
+namespace CppSharp.Passes
+{
+    public class CheckValueTypeClassesPass : TranslationUnitPass
+    {
+        public CheckValueTypeClassesPass()
+        {
+        }
+
+        public override bool VisitClassDecl(Class @class)
+        {
+            @class.Type = CheckClassIsStructible(@class, Driver) ? ClassType.ValueType : @class.Type;
+            return base.VisitClassDecl(@class);
+        }
+
+        private bool CheckClassIsStructible(Class @class, Driver Driver)
+        {
+            if (@class.IsUnion || @class.Namespace.Templates.Any(tmp => tmp.Name.Equals(@class.Name)))
+                return false;
+            if (@class.IsInterface || @class.IsStatic || @class.IsAbstract)
+                return false;
+            if (@class.Declarations.Any(decl => decl.Access == AccessSpecifier.Protected))
+                return false;
+            if (@class.Methods.Any(m => m.IsVirtual))
+                return false;
+            if (@class.HasBaseClass && @class.BaseClass.IsRefType)
+                return false;
+
+            var allTrUnits = Driver.ASTContext.TranslationUnits;
+            if (allTrUnits.Any(trUnit => trUnit.Classes.Any(
+                                  cls => cls.Bases.Any(clss => clss.IsClass && clss.Class == @class))))
+                return false;
+
+            if (@class.IsPOD || @class.IsValueType)
+                return true;
+
+            return false;
+        }
+    }
+}
