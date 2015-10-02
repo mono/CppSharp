9d390865e0d34b0d509e372d98c521cc10c3b547
Dimitar Dobrev <dpldobrev@yahoo.com>
2015-10-02 18:01:33 +0300

    Changed the premake to take multiple dependencies for test projects.
    Signed-off-by: Dimitar Dobrev <dpldobrev@yahoo.com>

--- tests/NamespacesDerived/NamespacesDerived.Tests.cs
+++ tests/NamespacesDerived/NamespacesDerived.Tests.cs
@@ -7,9 +7,8 @@ public class NamespaceDerivedTests
     [Test]
     public void TestNonRenamedMethod()
     {
-        // TODO: the premake is broken and does not add a reference to NamespaceBase.CSharp
-        //var derived = new Derived();
-        //var parent = derived.Parent;
-        //derived.parent(0);
+        var derived = new Derived();
+        var parent = derived.Parent;
+        derived.parent(0);
     }
 }
--- tests/NamespacesDerived/premake4.lua
+++ tests/NamespacesDerived/premake4.lua
@@ -2,3 +2,6 @@ group "Tests/Namespaces"
   SetupTestGeneratorProject("NamespacesDerived", "NamespacesBase")
   SetupTestNativeProject("NamespacesDerived",    "NamespacesBase")
   SetupTestProjectsCSharp("NamespacesDerived",   "NamespacesBase")
+  
+  project("NamespacesDerived.Tests.CSharp")
+    links { "NamespacesBase.CSharp" }
