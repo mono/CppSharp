4db8aa4af7e1a8c291ff9dc162c91b3eb83c8c4a
Joao Matos <joao@tritao.eu>
2015-11-29 04:04:52 +0000

    Remove extra MSVC warning C4141 from Clang build.

--- src/CppParser/premake4.lua
+++ src/CppParser/premake4.lua
@@ -2,7 +2,8 @@ clang_msvc_flags =
 {
   "/wd4146", "/wd4244", "/wd4800", "/wd4345",
   "/wd4355", "/wd4996", "/wd4624", "/wd4291",
-  "/wd4251"
+  "/wd4251",
+  "/wd4141", -- 'inline' : used more than once
 }
 
 if not (string.starts(action, "vs") and not os.is_windows()) then
