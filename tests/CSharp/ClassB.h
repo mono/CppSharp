6b28f97cb2b42ec2b4d2ff410164916aae9ece71
Abhinav Tripathi <genuineabhinav@gmail.com>
2016-02-18 21:57:41 +0530

    Just checkoing the issue

---
+++ tests/CSharp/ClassB.h
@@ -0,0 +1,5 @@
+#include "ClassY.h"
+
+class ClassB
+{
+};
---
+++ tests/CSharp/ClassY.h
@@ -0,0 +1,14 @@
+#ifndef __CLASS_Y__H__
+#define __CLASS_Y__H__
+
+class ClassY
+{
+public:
+	void init();
+};
+
+void ClassY::init()
+{
+}
+
+#endif
