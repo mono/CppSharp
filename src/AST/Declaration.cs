b117410729bd752a54cc5a9a334ba7cdbb4c3e6b
Joao Matos <joao@tritao.eu>
2016-10-25 12:37:24 +0100

    Moved typedef declarations to their own file.

--- src/AST/Declaration.cs
+++ src/AST/Declaration.cs
@@ -365,40 +365,6 @@ namespace CppSharp.AST
         public abstract T Visit<T>(IDeclVisitor<T> visitor);
     }
 
-    /// <summary>
-    /// Base class for declarations which introduce a typedef-name.
-    /// </summary>
-    public abstract class TypedefNameDecl : Declaration, ITypedDecl
-    {
-        public Type Type { get { return QualifiedType.Type; } }
-        public QualifiedType QualifiedType { get; set; }
-        public bool IsSynthetized { get; set; }
-    }
-
-    /// <summary>
-    /// Represents a type definition in C++.
-    /// </summary>
-    public class TypedefDecl : TypedefNameDecl
-    {
-        public override T Visit<T>(IDeclVisitor<T> visitor)
-        {
-            return visitor.VisitTypedefDecl(this);
-        }
-    }
-
-    /// <summary>
-    /// Represents a type alias in C++.
-    /// </summary>
-    public class TypeAlias : TypedefNameDecl
-    {
-        public TypeAliasTemplate DescribedAliasTemplate { get; set; }
-
-        public override T Visit<T>(IDeclVisitor<T> visitor)
-        {
-            return visitor.VisitTypeAliasDecl(this);
-        }
-    }
-
     public interface IDeclVisitor<out T>
     {
         T VisitDeclaration(Declaration decl);
