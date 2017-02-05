f2b7577b03c07e5cfbdbeb722335547f66a7fcd5
Joao Matos <joao@tritao.eu>
2017-02-05 23:53:32 +0000

    Fixed NUnit paths in Nuget install script.

--- build/InstallNugets.sh
+++ build/InstallNugets.sh
@@ -1,4 +1,5 @@
 wget https://nuget.org/nuget.exe
-mono nuget.exe install NUnit -Version 2.6.4 -OutputDirectory ../deps
-mono nuget.exe install NUnit.Runners -Version 2.6.4 -OutputDirectory ../deps
-cp ../deps/NUnit.2.6.4/lib/nunit.framework.* deps/NUnit/ 
+BUILD_DIR=$(dirname -- $0)
+mono nuget.exe install NUnit -Version 2.6.4 -OutputDirectory $BUILD_DIR/../deps
+mono nuget.exe install NUnit.Runners -Version 2.6.4 -OutputDirectory $BUILD_DIR/../deps
+cp $BUILD_DIR/../deps/NUnit.2.6.4/lib/nunit.framework.* $BUILD_DIR/../deps/NUnit
