ed08714e9c8851266afd5e0f39af41e74d7b8977
Joao Matos <joao@tritao.eu>
2017-02-06 18:53:42 +0000

    Run the AST tests under Travis.

--- build/RunTests.sh
+++ build/RunTests.sh
@@ -1,4 +1,4 @@
 set -e
 BUILD_DIR=$(dirname -- $0)
 MONO_PATH=$BUILD_DIR/../deps/NUnit.Runners.2.6.4/tools \
-mono $BUILD_DIR/../deps/NUnit.Runners.2.6.4/tools/nunit-console.exe -nologo -noresult $BUILD_DIR/gmake/lib/Release_*/*Tests.*.dll
+mono $BUILD_DIR/../deps/NUnit.Runners.2.6.4/tools/nunit-console.exe -nologo -noresult $BUILD_DIR/gmake/lib/Release_*/*Tests*.dll
