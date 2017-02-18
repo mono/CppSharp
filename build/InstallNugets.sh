745f5fc8ea655197c8b79fda580346db8cd4a0f4
Dimitar Dobrev <dpldobrev@protonmail.com>
2017-02-18 13:59:50 +0200

    Fixed the path to the new NUnit in the build scripts.
    Signed-off-by: Dimitar Dobrev <dpldobrev@protonmail.com>

--- build/InstallNugets.sh
+++ build/InstallNugets.sh
@@ -2,4 +2,5 @@ wget https://nuget.org/nuget.exe
 BUILD_DIR=$(dirname -- $0)
 mono nuget.exe install NUnit -Version 3.6.0 -OutputDirectory $BUILD_DIR/../deps
 mono nuget.exe install NUnit.Runners -Version 3.6.0 -OutputDirectory $BUILD_DIR/../deps
-cp $BUILD_DIR/../deps/NUnit.3.6.0/lib/nunit.framework.* $BUILD_DIR/../deps/NUnit
+ls -d $BUILD_DIR/../deps/*/
+cp $BUILD_DIR/../deps/NUnit/lib/net45/nunit.framework.* $BUILD_DIR/../deps/NUnit
