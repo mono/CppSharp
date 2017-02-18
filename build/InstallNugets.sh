200aa1f84ece2ace5b63b7d8bb5dd53dd55c5934
Dimitar Dobrev <dpldobrev@protonmail.com>
2017-02-18 14:46:59 +0200

    Removed needless copying of NUnit libraries.
    Signed-off-by: Dimitar Dobrev <dpldobrev@protonmail.com>

--- build/InstallNugets.sh
+++ build/InstallNugets.sh
@@ -1,6 +1,3 @@
 wget https://nuget.org/nuget.exe
 BUILD_DIR=$(dirname -- $0)
-mono nuget.exe install NUnit -Version 3.6.0 -OutputDirectory $BUILD_DIR/../deps
-mono nuget.exe install NUnit.Runners -Version 3.6.0 -OutputDirectory $BUILD_DIR/../deps
-ls -d $BUILD_DIR/../deps/*/
-cp $BUILD_DIR/../deps/NUnit/lib/net45/nunit.framework.* $BUILD_DIR/../deps/NUnit
+mono nuget.exe install NUnit.ConsoleRunner -Version 3.6.0 -OutputDirectory $BUILD_DIR/../deps
--- build/RunTests.sh
+++ build/RunTests.sh
@@ -1,4 +1,5 @@
 set -e
 BUILD_DIR=$(dirname -- $0)
-MONO_PATH=$BUILD_DIR/../deps/NUnit.Runners.2.6.4/tools \
-mono $BUILD_DIR/../deps/NUnit.Runners.2.6.4/tools/nunit-console.exe -nologo -noresult $BUILD_DIR/gmake/lib/Release_*/*Tests*.dll
+MONO_PATH=$BUILD_DIR/../deps/NUnit.ConsoleRunner.3.6.0/tools \
+cp $BUILD_DIR/../deps/NUnit/nunit.framework.* $BUILD_DIR/gmake/lib/Release_*/
+mono $BUILD_DIR/../deps/NUnit.ConsoleRunner.3.6.0/tools/nunit3-console.exe -noresult $BUILD_DIR/gmake/lib/Release_*/*Tests*.dll
