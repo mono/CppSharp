f2e5834ae835fc014b5102f5fa8068eff7610e9b
Joao Matos <joao@tritao.eu>
2017-02-18 19:04:47 +0000

    Update Mono install script from version 4.2.1 to version 4.6.2.

--- build/InstallMono.sh
+++ build/InstallMono.sh
@@ -1,6 +1,6 @@
 OS=$(uname -s)
 if [ "$OS" == "Darwin" ]; then
-	wget -O mono.pkg http://download.mono-project.com/archive/4.2.1/macos-10-x86/MonoFramework-MDK-4.2.1.102.macos10.xamarin.x86.pkg
+	wget -O mono.pkg https://download.mono-project.com/archive/4.6.2/macos-10-universal/MonoFramework-MDK-4.6.2.16.macos10.xamarin.universal.pkg
 	sudo installer -pkg mono.pkg -target /
 elif [ "$OS" == "Linux" ]; then
 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
