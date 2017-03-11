6ed451cdcd4e3ba8883149a94d9a988621823246
realvictorprm <mueller.vpr@gmail.com>
2017-03-11 17:01:10 +0100

    Revert "Added support for VS 2017 solution building."
    This reverts commit 204992886890af51cb5eb3c9b7458017138c0988.

--- build/GenerateProjects.bat
+++ build/GenerateProjects.bat
@@ -8,15 +8,12 @@ echo [0] Clean
 echo [1] Visual C++ 2012
 echo [2] Visual C++ 2013
 echo [3] Visual C++ 2015
-echo [4] Visual C++ 2017
-echo [5] GNU Make
-
+echo [4] GNU Make
 echo.
 
 :choice
 set /P C="Choice: "
-if "%C%"=="5" goto gmake
-if "%C%"=="4" goto vs2017
+if "%C%"=="4" goto gmake
 if "%C%"=="3" goto vs2015
 if "%C%"=="2" goto vs2013
 if "%C%"=="1" goto vs2012
@@ -36,10 +33,6 @@ goto quit
 
 :vs2015
 "premake5" --file=premake5.lua vs2015
-goto quit
-
-:vs2017
-"premake5" --file=premake5.lua vs2017 
 goto quit
 
 :gmake
--- build/scripts/Build.lua
+++ build/scripts/Build.lua
@@ -1,69 +1,72 @@
 require "Utils"
 
 function get_msbuild_path()
-    local msbuild = '%SystemRoot%\\system32\\reg.exe query "HKLM\\SOFTWARE\\Microsoft\\MSBuild\\ToolsVersions\\4.0" /v MSBuildToolsPath'
-    local val = execute(msbuild)
-    
-    for i in string.gmatch(val, "%S+") do
-        if os.isdir(i) then
-            return i
-        end
-    end
-    
-    error("MSBuild path could not be found in Windows registry.")
+	local msbuild = '%SystemRoot%\\system32\\reg.exe query "HKLM\\SOFTWARE\\Microsoft\\MSBuild\\ToolsVersions\\4.0" /v MSBuildToolsPath'
+	local val = execute(msbuild)
+
+	for i in string.gmatch(val, "%S+") do
+		if os.isdir(i) then
+			return i
+		end
+	end
+
+	error("MSBuild path could not be found in Windows registry.")
 end
 
 function msbuild(sln, conf)
-    local msbuild_path = path.normalize(path.join(get_msbuild_path(), "msbuild.exe"))
-    local sln = path.normalize(sln)
-    
-    local cmd = msbuild_path .. " " .. sln
-    if conf ~= nil then
-        cmd = cmd .. " /p:Configuration=" .. conf
-    end
-    
-    execute_or_die(cmd)
+	local msbuild_path = path.normalize(path.join(get_msbuild_path(), "msbuild.exe"))
+	local sln = path.normalize(sln)
+
+	local cmd = msbuild_path .. " " .. sln
+	if conf ~= nil then
+		cmd = cmd .. " /p:Configuration=" .. conf
+	end
+
+	execute_or_die(cmd)
 end
 
 function ninja(dir, action)
-    local cmd = "ninja -C " .. dir
-    if action then
-        cmd = cmd .. " " .. action
-    end
-    return execute_or_die(cmd)
+	local cmd = "ninja -C " .. dir
+	if action then
+		cmd = cmd .. " " .. action
+	end
+	return execute_or_die(cmd)
 end
 
 function run_premake(file, action)
-    -- search for file with extension Lua
-    if os.isfile(file .. ".lua") then
-        file = file .. ".lua"
-    end
-    
-    local cmd = string.format("%s --file=%s %s", _PREMAKE_COMMAND, file, action)
-    return execute_or_die(cmd)
+	-- search for file with extension Lua
+	if os.isfile(file .. ".lua") then
+		file = file .. ".lua"
+	end
+
+	local cmd = string.format("%s --file=%s %s", _PREMAKE_COMMAND, file, action)
+	return execute_or_die(cmd)
 end
 
 function build_cppsharp()
-    -- install dependencies
-    run_premake("Provision", "provision")
-    
-    -- if there is an llvm git clone then use it
-    -- otherwise download and extract llvm/clang build
-    run_premake("LLVM", "download_llvm")
-    
-    -- generate project files
-    run_premake("../premake4", "gmake")
+	-- install dependencies
+	run_premake("Provision", "provision")
 
--- build cppsharp
---[[BUILD_CONF=release_x32;
-config=$BUILD_CONF make -C build/gmake/
-BUILD_DIR=`ls build/gmake/lib`
-mkdir -p "$PWD"/build/gmake/lib/lib/"$BUILD_DIR"
-cp "$PWD"/build/gmake/lib/"$BUILD_DIR"/libNamespacesBase.* "$PWD"/build/gmake/lib/lib/"$BUILD_DIR"
-]]
+	-- if there is an llvm git clone then use it
+	-- otherwise download and extract llvm/clang build
+
+	run_premake("LLVM", "download_llvm")
+
+	-- generate project files
+	run_premake("../premake4", "gmake")
+
+	-- build cppsharp
+	--[[BUILD_CONF=release_x32;
+	config=$BUILD_CONF make -C build/gmake/
+	BUILD_DIR=`ls build/gmake/lib`
+	mkdir -p "$PWD"/build/gmake/lib/lib/"$BUILD_DIR"
+	cp "$PWD"/build/gmake/lib/"$BUILD_DIR"/libNamespacesBase.* "$PWD"/build/gmake/lib/lib/"$BUILD_DIR"
+	]]
 end
 
 if _ACTION == "build_cppsharp" then
-    build_cppsharp()
-    os.exit()
+  build_cppsharp()
+  os.exit()
 end
+
+
