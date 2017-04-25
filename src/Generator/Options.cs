ac1c25a848e9e37eb6f52dccfde5115e03f11db7
Joao Matos <joao@tritao.eu>
2017-04-25 13:21:14 +0100

    Remove Embeddinator-specific options from CppSharp.

--- src/Generator/Options.cs
+++ src/Generator/Options.cs
@@ -195,18 +195,6 @@ namespace CppSharp
         public HashSet<string> ExplicitlyPatchedVirtualFunctions { get; }
 
         #endregion
-
-        #region Embeddinator options
-
-        // If true, will use unmanaged->managed thunks to call managed methods.
-        // In this mode the JIT will generate specialized wrappers for marshaling
-        // which will be faster but also lead to higher memory consumption.
-        public bool UseUnmanagedThunks;
-
-        // If true, will generate support files alongside generated binding code.
-        public bool GenerateSupportFiles = true;
-
-        #endregion
     }
 
     public class InvalidOptionException : Exception
