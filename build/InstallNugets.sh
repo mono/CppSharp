5a289d0f00333ec028c942f0dda54f3391d8561f
Dimitar Dobrev <dpldobrev@protonmail.com>
2017-04-26 13:05:11 +0300

    Updated the NUnit used in Travis in order to fix a crash in it.
    Signed-off-by: Dimitar Dobrev <dpldobrev@protonmail.com>

--- build/InstallNugets.sh
+++ build/InstallNugets.sh
@@ -1,3 +1,3 @@
 wget https://nuget.org/nuget.exe
 BUILD_DIR=$(dirname -- $0)
-mono nuget.exe install NUnit.ConsoleRunner -Version 3.6.0 -OutputDirectory $BUILD_DIR/../deps
+mono nuget.exe install NUnit.ConsoleRunner -Version 3.6.1 -OutputDirectory $BUILD_DIR/../deps
--- build/RunTests.sh
+++ build/RunTests.sh
@@ -1,5 +1,5 @@
 set -e
 BUILD_DIR=$(dirname -- $0)
-MONO_PATH=$BUILD_DIR/../deps/NUnit.ConsoleRunner.3.6.0/tools \
+MONO_PATH=$BUILD_DIR/../deps/NUnit.ConsoleRunner.3.6.1/tools \
 cp $BUILD_DIR/../deps/NUnit/nunit.framework.* $BUILD_DIR/gmake/lib/Release_*/
-mono $BUILD_DIR/../deps/NUnit.ConsoleRunner.3.6.0/tools/nunit3-console.exe --agents=1 -noresult $BUILD_DIR/gmake/lib/Release_*/*Tests*.dll
+mono $BUILD_DIR/../deps/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe -noresult $BUILD_DIR/gmake/lib/Release_*/*Tests*.dll
