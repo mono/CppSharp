96beeeada3233177dc26190d698ec4a306cdf2f3
Mohit Mohta <mohtamohit@gmail.com>
2017-06-10 20:05:14 +0530

    Added Generation of Std-templates Project (#859)

--- src/CppParser/Bindings/CSharp/x86_64-linux-gnu/Std-templates.cpp
+++ src/CppParser/Bindings/CSharp/x86_64-linux-gnu/Std-templates.cpp
@@ -3,6 +3,6 @@
 #include <map>
 #include <unordered_map>
 
-template class __declspec(dllexport) std::allocator<char>;
-template class __declspec(dllexport) std::char_traits<char>;
-template class __declspec(dllexport) std::basic_string<char, std::char_traits<char>, std::allocator<char>>;
+template class std::allocator<char>;
+template class std::char_traits<char>;
+template class std::basic_string<char, std::char_traits<char>, std::allocator<char>>;
