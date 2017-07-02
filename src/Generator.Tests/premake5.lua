2692839fba8892130b57375bb494c17397161ff0
ktopouzi <it123835@it.teithe.gr>
2017-07-02 20:02:26 +0300

    Included the headers for AST tests to the solution.
    Fixes #850

--- src/Generator.Tests/premake5.lua
+++ src/Generator.Tests/premake5.lua
@@ -6,12 +6,17 @@ project "CppSharp.Generator.Tests"
   files { "**.cs" }
   vpaths { ["*"] = "*" }
 
-  libdirs 
+  libdirs
   {
     depsdir .. "/NUnit",
     depsdir .. "/NSubstitute"
   }
-  
+
+  files { testsdir .. "/Native/AST.h", testsdir .. "/Native/ASTExtensions.h", testsdir .. "/Native/Passes.h" }
+  filter "files:**.h"
+     buildaction "None"
+  filter {}
+
   SetupParser()
 
   links
