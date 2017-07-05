709f431f9ed018fe688ccd19dd1d8012b417be84
Joao Matos <joao@tritao.eu>
2017-07-05 11:19:13 +0100

    Upgrade the Vagrant and provisioning script to Ubuntu 16.06.

--- build/scripts/Provision.lua
+++ build/scripts/Provision.lua
@@ -60,8 +60,8 @@ local compile_llvm = is_vagrant()
 
 function provision_linux()
 	-- Add Repos
-	sudo("apt-key adv --keyserver http://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF")
-	sudo("echo \"deb http://download.mono-project.com/repo/debian wheezy main\" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list")
+	sudo("apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF")
+	sudo("echo \"deb http://download.mono-project.com/repo/ubuntu xenial main\" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list")
 
 	sudo("apt-get update")
 
