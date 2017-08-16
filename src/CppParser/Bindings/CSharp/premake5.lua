7d6c6141e3f8a287a469a959fc0a7fd7aed319e5
Joao Matos <joao@tritao.eu>
2017-08-16 12:50:36 +0100

    Move Std-symbols to the CppParser project build files.

--- src/CppParser/Bindings/CSharp/premake5.lua
+++ src/CppParser/Bindings/CSharp/premake5.lua
@@ -32,37 +32,6 @@ project "CppSharp.Parser.CSharp"
 
   filter {}
 
-project "Std-symbols"
-
-  kind "SharedLib"
-  language "C++"
-  SetupNativeProject()
-  rtti "Off"
-
-  filter { "action:vs*" }
-    buildoptions { clang_msvc_flags }
-
-  filter {}
-
-  if os.istarget("windows") then
-      files { "i686-pc-win32-msvc/Std-symbols.cpp" }
-  elseif os.istarget("macosx") then
-      local file = io.popen("lipo -info `which mono`")
-      local output = file:read('*all')
-      if string.find(output, "x86_64") then
-        files { "x86_64-apple-darwin12.4.0/Std-symbols.cpp" }
-      else
-        files { "i686-apple-darwin12.4.0/Std-symbols.cpp" }
-      end
-
-  elseif os.istarget("linux") then
-      files { "x86_64-linux-gnu/Std-symbols.cpp" }
-  else
-      print "Unknown architecture"
-  end
-
-  filter {}
-
 function SetupParser()
   links
   {
