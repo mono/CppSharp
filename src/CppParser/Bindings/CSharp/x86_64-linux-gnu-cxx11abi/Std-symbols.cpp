aed21bed677805aa6ca33276315fa0925a1154fd
Dimitar Dobrev <dpldobrev@protonmail.com>
2017-11-21 15:39:23 +0200

    Included the destructor of std::allocator to the C++ symbols.
    Signed-off-by: Dimitar Dobrev <dpldobrev@protonmail.com>

--- src/CppParser/Bindings/CSharp/x86_64-linux-gnu-cxx11abi/Std-symbols.cpp
+++ src/CppParser/Bindings/CSharp/x86_64-linux-gnu-cxx11abi/Std-symbols.cpp
@@ -2,6 +2,7 @@
 
 
 template std::allocator<char>::allocator();
+template std::allocator<char>::~allocator();
 template std::basic_string<char, std::char_traits<char>, std::allocator<char>>::basic_string(const char*, const std::allocator<char>&);
 template std::basic_string<char, std::char_traits<char>, std::allocator<char>>::~basic_string();
 template const char* std::basic_string<char, std::char_traits<char>, std::allocator<char>>::c_str() const noexcept;
