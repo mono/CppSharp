eaf6a8e45091b053b54408569ef097ff7da52b90
Dimitar Dobrev <dpldobrev@protonmail.com>
2018-12-21 23:11:34 +0200

    Decreased the build time on the Linux CI.
    Signed-off-by: Dimitar Dobrev <dpldobrev@protonmail.com>

--- build/InstallNugets.sh
+++ build/InstallNugets.sh
@@ -1,3 +1,3 @@
-wget https://nuget.org/nuget.exe
+wget https://github.com/nunit/nunit-console/releases/download/v3.9/NUnit.Console-3.9.0.zip
 BUILD_DIR=$(dirname -- $0)
-mono nuget.exe install NUnit.ConsoleRunner -Version 3.6.1 -OutputDirectory $BUILD_DIR/../deps
+unzip NUnit.Console-3.9.0.zip -d $BUILD_DIR/../deps/NUnit.Console-3.9.0
--- build/RunTests.sh
+++ build/RunTests.sh
@@ -2,4 +2,4 @@ set -e
 BUILD_DIR=$(dirname -- $0)
 MONO_PATH=$BUILD_DIR/../deps/NUnit.ConsoleRunner.3.6.1/tools \
 cp $BUILD_DIR/../deps/NUnit/nunit.framework.* $BUILD_DIR/gmake/lib/Release_*/
-mono $BUILD_DIR/../deps/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe -noresult $BUILD_DIR/gmake/lib/Release_*/*Tests*.dll
+mono $BUILD_DIR/../deps/NUnit.Console-3.9.0/nunit3-console.exe -noresult $BUILD_DIR/gmake/lib/Release_*/*Tests*.dll
