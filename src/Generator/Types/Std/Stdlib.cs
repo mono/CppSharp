49ab0227f98c7c625c470c6b5ec2479d77772917
Dimitar Dobrev <dpldobrev@protonmail.com>
2019-07-02 03:01:13 +0300

    Fix a memory leak when passing an indirect std::string
    Signed-off-by: Dimitar Dobrev <dpldobrev@protonmail.com>

--- src/Generator/Types/Std/Stdlib.cs
+++ src/Generator/Types/Std/Stdlib.cs
@@ -324,7 +324,7 @@ namespace CppSharp.Types.Std
                 ctx.Before.WriteLine($@"{qualifiedBasicString}Extensions.{
                     assign.Name}({varBasicString}, {ctx.Parameter.Name});");
                 ctx.Return.Write($"{varBasicString}.{Helpers.InstanceIdentifier}");
-                if (!type.IsPointer())
+                if (!type.IsPointer() || ctx.Parameter.IsIndirect)
                     ctx.Cleanup.WriteLine($@"{varBasicString}.Dispose({
                         (ctx.MarshalKind == MarshalKind.NativeField ? "false" : string.Empty)});");
             }
