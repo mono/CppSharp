3dd467626cf3f9a0222373dfc53c340eb6e5192d
Dimitar Dobrev <dpldobrev@protonmail.com>
2020-07-24 16:16:09 +0300

    Update the Mono used in the CI for Linux
    Signed-off-by: Dimitar Dobrev <dpldobrev@protonmail.com>

--- build/InstallMono.sh
+++ build/InstallMono.sh
@@ -6,7 +6,7 @@ if [ "$OS" == "Darwin" ]; then
 elif [ "$OS" == "Linux" ]; then
 	sudo apt install gnupg ca-certificates
 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-	echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+	echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 	sudo apt-get update -qq
 	sudo apt-get install -y --force-yes mono-mcs libmono-system-runtime4.0-cil
 fi
