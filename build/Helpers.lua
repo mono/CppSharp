4aefe6f9ec9b6b6174091440a7b1c73677adbcd9
josetr <37419832+josetr@users.noreply.github.com>
2020-10-17 22:24:47 +0100

    Add missing parentheses

--- build/Helpers.lua
+++ build/Helpers.lua
@@ -164,7 +164,7 @@ end
 
 function SetupManagedProject()
   language "C#"
-  csversion "7.2"
+  csversion ("7.2")
   location ("%{wks.location}/projects")
   buildoptions {"/platform:".._OPTIONS["arch"]}
 
