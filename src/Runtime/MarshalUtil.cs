0cfc06001812a98d3149399649f8beec02f14303
josetr <37419832+josetr@users.noreply.github.com>
2020-10-23 19:41:08 +0100

    Fix string marshalling bug and move hard to read code to CppSharp.Runtime (#1445)

---
+++ src/Runtime/MarshalUtil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace CppSharp.Runtime
+{
+    public unsafe static class MarshalUtil
+    {
+        public static string GetString(Encoding encoding, IntPtr str)
+        {
+            if (str == IntPtr.Zero)
+                return null;
+
+            int byteCount = 0;
+
+            if (encoding == Encoding.UTF32)
+            {
+                var str32 = (int*)str;
+                while (*(str32++) != 0) byteCount += sizeof(int);
+            }
+            else if (encoding == Encoding.Unicode || encoding == Encoding.BigEndianUnicode)
+            {
+                var str16 = (short*)str;
+                while (*(str16++) != 0) byteCount += sizeof(short);
+            }
+            else
+            {
+                var str8 = (byte*)str;
+                while (*(str8++) != 0) byteCount += sizeof(byte);
+            }
+
+            return encoding.GetString((byte*)str, byteCount);
+        }
+    }
+}
