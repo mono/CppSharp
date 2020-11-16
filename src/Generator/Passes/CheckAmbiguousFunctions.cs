d0f2213a897f67d2a2b07677667f50f1b0f11916
Joao Matos <joao@tritao.eu>
2020-11-16 18:47:09 +0000

    Minor formatting fix.

--- src/Generator/Passes/CheckAmbiguousFunctions.cs
+++ src/Generator/Passes/CheckAmbiguousFunctions.cs
@@ -88,6 +88,7 @@ namespace CppSharp.Passes
 
                 if (!funcType.Equals(functionParams[i].Type.Desugar()))
                     functionMappedParams++;
+
                 if (!overloadType.Equals(overloadParams[i].Type.Desugar()))
                     overloadMappedParams++;
 
