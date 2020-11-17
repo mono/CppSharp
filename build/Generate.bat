a86ca32e8f974d5f1e5c3f6a7dea04c80287d2dc
josetr <37419832+josetr@users.noreply.github.com>
2020-11-17 01:02:39 +0000

    Remove unsupported Visual Studio versions from generate options

--- build/Generate.bat
+++ build/Generate.bat
@@ -5,47 +5,29 @@ goto menu
 echo Build Project Generator:
 echo.
 echo [0] Clean
-echo [1] Visual C++ 2012
-echo [2] Visual C++ 2013
-echo [3] Visual C++ 2015
-echo [4] Visual C++ 2017
-echo [5] Visual C++ 2019
-echo [6] GNU Make
+echo [1] Visual C++ 2019
+echo [2] Visual C++ 2017
+echo [3] GNU Make
 
 echo.
 
 :choice
 set /P C="Choice: "
-if "%C%"=="6" goto gmake
-if "%C%"=="5" goto vs2019
-if "%C%"=="4" goto vs2017
-if "%C%"=="3" goto vs2015
-if "%C%"=="2" goto vs2013
-if "%C%"=="1" goto vs2012
 if "%C%"=="0" goto clean
+if "%C%"=="1" goto vs2019
+if "%C%"=="2" goto vs2017
+if "%C%"=="3" goto gmake
 
 :clean
 "premake5" --file=premake5.lua clean
 goto quit
 
-:vs2012
-"premake5" --file=premake5.lua vs2012
-goto quit
-
-:vs2013
-"premake5" --file=premake5.lua vs2013
-goto quit
-
-:vs2015
-"premake5" --file=premake5.lua vs2015
+:vs2019
+"premake5" --file=premake5.lua vs2019
 goto quit
 
 :vs2017
 "premake5" --file=premake5.lua vs2017 
-goto quit
-
-:vs2019
-"premake5" --file=premake5.lua vs2019
 goto quit
 
 :gmake
