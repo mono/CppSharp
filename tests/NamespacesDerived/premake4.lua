2b763c198302ccba6e79c6d8dbe5ee5020d653f8
josetr <37419832+josetr@users.noreply.github.com>
2020-11-23 13:43:06 +0000

    Remove whitespaces
    Remove whitespaces

--- tests/NamespacesDerived/premake4.lua
+++ tests/NamespacesDerived/premake4.lua
@@ -2,6 +2,6 @@ group "Tests/Namespaces"
   SetupTestGeneratorProject("NamespacesDerived")
   SetupTestNativeProject("NamespacesDerived", "NamespacesBase")
   SetupTestProjectsCSharp("NamespacesDerived", "NamespacesBase")
-  
+
   project("NamespacesDerived.Tests.CSharp")
     links { "NamespacesBase.CSharp" }
