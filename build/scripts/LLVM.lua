4b2a8ba68d8d7e2c7b3a62db12ae925abedf30f9
josetr <37419832+josetr@users.noreply.github.com>
2020-11-25 00:58:30 +0000

    Remove secondary Zstandard compression from LLVM packages

--- build/scripts/LLVM.lua
+++ build/scripts/LLVM.lua
@@ -469,10 +469,7 @@ function archive_llvm(dir)
 	else
 		execute_or_die("tar cJf " .. path.join("..", archive) .. " *")
 	end
-
-	archive = dir .. ".tar.zst"
-	execute_or_die("tar --use-compress-program zstd -cf " .. path.join("..", archive) .. " *")
-
+	
 	os.chdir(cwd)
 	if is_vagrant() then
 		os.copyfile(archive, "/cppsharp")
