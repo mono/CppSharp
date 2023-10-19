89813f107451d1416ec4ee6858b46af0b8a37d36
Salvage <29021710+Saalvage@users.noreply.github.com>
2023-10-19 16:36:42 +0200

    Remove redundant usings

--- tests/dotnet/CSharp/CSharp.Tests.cs
+++ tests/dotnet/CSharp/CSharp.Tests.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
 using CSharp;
 using NUnit.Framework;
-using ValueType = CSharp.ValueType;
 
 [TestFixture]
 public unsafe class CSharpTests
