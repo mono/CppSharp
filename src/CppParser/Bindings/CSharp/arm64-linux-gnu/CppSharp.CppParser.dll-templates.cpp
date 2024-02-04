59ff7266e130ab5f7ec54b1c23a0b023c3ceb952
Conrad Kreyling <conrad@kreyling.biz>
2024-02-03 23:43:32 -0500

    Copy templates.cpp to arm folders as well

---
+++ src/CppParser/Bindings/CSharp/arm64-linux-gnu/CppSharp.CppParser.dll-templates.cpp
@@ -0,0 +1,30 @@
+#include <AST.h>
+#include <Sources.h>
+#include <CppParser.h>
+
+template class vector<CppSharp::CppParser::AST::Namespace*>;
+template class vector<CppSharp::CppParser::AST::Enumeration*>;
+template class vector<CppSharp::CppParser::AST::Function*>;
+template class vector<CppSharp::CppParser::AST::Class*>;
+template class vector<CppSharp::CppParser::AST::Template*>;
+template class vector<CppSharp::CppParser::AST::TypedefDecl*>;
+template class vector<CppSharp::CppParser::AST::TypeAlias*>;
+template class vector<CppSharp::CppParser::AST::Variable*>;
+template class vector<CppSharp::CppParser::AST::Friend*>;
+template class vector<CppSharp::CppParser::AST::BaseClassSpecifier*>;
+template class vector<CppSharp::CppParser::AST::Field*>;
+template class vector<CppSharp::CppParser::AST::Method*>;
+template class vector<CppSharp::CppParser::AST::AccessSpecifierDecl*>;
+template class vector<CppSharp::CppParser::AST::Declaration*>;
+template class vector<CppSharp::CppParser::AST::FunctionTemplateSpecialization*>;
+template class vector<CppSharp::CppParser::AST::Parameter*>;
+template class vector<CppSharp::CppParser::AST::Enumeration::Item*>;
+template class vector<CppSharp::CppParser::AST::BlockContentComment*>;
+template class vector<CppSharp::CppParser::AST::PreprocessedEntity*>;
+template class vector<CppSharp::CppParser::AST::Expression*>;
+template class vector<CppSharp::CppParser::AST::ClassTemplateSpecialization*>;
+template class vector<CppSharp::CppParser::AST::VarTemplateSpecialization*>;
+template class vector<CppSharp::CppParser::AST::MacroDefinition*>;
+template class vector<CppSharp::CppParser::AST::TranslationUnit*>;
+template class vector<CppSharp::CppParser::AST::InlineContentComment*>;
+template class vector<CppSharp::CppParser::AST::VerbatimBlockLineComment*>;
